1da70360b862deefa84b812af36dd21a0e0050df
Carson Lee <46005255+CarsonLeee@users.noreply.github.com>
2021-09-26 15:05:43 -0400

    Add files via upload

---
+++ Assignment 3/src/Edge.java
@@ -0,0 +1,29 @@
+///Class that represents an Edge
+public class Edge {
+	
+	//Fields of the class
+	private Node first, second;
+	private String busLine;
+	
+	//Constructor of the class
+	public Edge(Node u, Node v, String busLine) {
+		this.first = u;
+		this.second = v;
+		this.busLine = busLine;
+	}
+	
+	//Getter for first endpoint
+	public Node firstEndpoint() {
+		return first;
+	}
+	
+	//Getter for second endpoint
+	public Node secondEndpoint() {
+		return second;
+	}
+	
+	//Getter for bus line
+	public String getBusLine() {
+		return busLine;
+	}
+}
